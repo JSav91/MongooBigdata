d114a03e8e52bb70757b4336dfbb2287e024b6b6
Joel Savio <savio.joel91@gmail.com>
2017-05-06 12:32:19 +1200

    delete and update

--- src/main/java/com/unitec/ade/business/DataDeleteManager.java
+++ src/main/java/com/unitec/ade/business/DataDeleteManager.java
@@ -1,12 +1,27 @@
 package com.unitec.ade.business;
 
-public class DataDeleteManager {
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.DeleteResult;
+import com.unitec.ade.constants.ApplicationConstants;
+
+public class DataDeleteManager implements ApplicationConstants{
 
 	public static Long totalTimeforRead=new Long(0);
 	public static Long rowcount=new Long(0);
 	public static Long size= new Long(0);
 	
+	//Delete all cases before 1990 which have been solved
 	public void runQuery(){
 		
+		long startTime=System.currentTimeMillis();
+
+		DeleteResult deleteResult=DB.getCollection("case").deleteMany(Filters.and(Filters.eq("status","solved"),Filters.lt("date_time","1990/01/01")));
+		
+		long stopTime=System.currentTimeMillis();
+		
+		totalTimeforRead=(stopTime-startTime);
+		
+		System.out.println("Deleted rows count"+deleteResult.getDeletedCount());
+		System.out.println("Total time taken "+totalTimeforRead +"ms\n\n\n");
 	}
 }
--- src/main/java/com/unitec/ade/business/DataUpdateManager.java
+++ src/main/java/com/unitec/ade/business/DataUpdateManager.java
@@ -1,6 +1,11 @@
 package com.unitec.ade.business;
 
-public class DataUpdateManager {
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.UpdateResult;
+import com.unitec.ade.constants.ApplicationConstants;
+
+public class DataUpdateManager implements ApplicationConstants{
 
 	
 	public static Long totalTimeforRead=new Long(0);
@@ -9,5 +14,17 @@ public class DataUpdateManager {
 	
 	public void runQuery(){
 		
+		long startTime=System.currentTimeMillis();
+
+		UpdateResult updateResult=DB.getCollection("case").updateMany(
+				(Filters.and(Filters.eq("status","solved"),Filters.gt("date_time","1990/01/01"),Filters.lt("date_time","1991/01/01")))
+				,Updates.combine(Updates.set("status","unsolved"))) ;
+		
+		long stopTime=System.currentTimeMillis();
+		
+		totalTimeforRead=(stopTime-startTime);
+		
+		System.out.println("Updated rows count"+updateResult.getModifiedCount());
+		System.out.println("Total time taken "+totalTimeforRead +"ms\n\n\n");
 	}
 }
